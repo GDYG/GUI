135ec1dee3f1871a17edddfe24b58e00f0f10e53
anyahui <1549472324@qq.com>
2021-07-26 15:59:54 +0800

    titles update

--- .umirc.ts
+++ .umirc.ts
@@ -8,7 +8,7 @@ if (process.env.SITE_BUILD_ENV === 'PREVIEW') {
   publicPath = undefined;
 }
 export default defineConfig({
-    title: 'G-UI', // 站点名称
+    title: 'GUI', // 站点名称
     mode: 'site',
     outputPath: 'doc-site', // 输出文件夹
     exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
