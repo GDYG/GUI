e5ea1ea2b2714b5b27c12c06725683168ee6acad
anyahui <1549472324@qq.com>
2021-07-27 14:35:29 +0800

    v1.0.3

--- scripts/release.ts
+++ scripts/release.ts
@@ -115,7 +115,7 @@ async function push(nextVersion: string) {
  */
 async function build() {
   // timeLog('组件库打包', 'start');
-  await run('npm run build');
+  await run('yarn build');
   // timeLog('组件库打包', 'end');
 }
 
