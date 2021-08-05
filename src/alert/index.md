6b4a2a2df8eb2046a2aabe87c93923fcd5dce34d
anyahui <1549472324@qq.com>
2021-08-05 16:56:18 +0800

    v0.0.2

--- src/alert/index.md
+++ src/alert/index.md
@@ -12,10 +12,23 @@ group:
 
 警告提示，展现需要关注的信息。
 
+# 何时使用
+
+* 当某个页面需要向用户显示警告的信息时。
+
 ## 代码演示
 
 ### 基本用法
 
-<code src="./demo/basic.tsx"></code>
+<code src="./demo/basic.tsx" title='四种样式' desc='共有四种样式info、positive、negative、warning。'></code>
+
+### 可关闭的警告提示
+<code src="./demo/basicClose.tsx" title='带有关闭按钮' desc='关闭按钮, 默认不显示'></code>
+
+### 自定义图标
+<code src="./demo/basicIcon.tsx" title='自定义前缀图标' desc='支持自定义图标, 可以是图片、svg、canvas等'></code>
+
+
+
 <API src="./index.tsx"></API>
 
--- src/alert/index.tsx
+++ src/alert/index.tsx
@@ -1,38 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import t from 'prop-types';
-
 export interface AlertProps {
 /**
    * @description       Alert 的类型
    * @default           'info'
 */
+  icon?: React.ReactNode,
+  closed?: boolean,
   kind?: 'info' | 'positive' | 'negative' | 'warning';
 }
 
 export type KindMap = Record<Required<AlertProps>['kind'], string>;
 
-const prefixCls = 'happy-alert';
+const prefixCls = 'gnt-alert';
+const prefixClsPrev = 'gnt-alert-prev';
+const closeCls = 'gnt-alert-close';
 
 const kinds: KindMap = {
   info: '#5352ED',
-  positive: '#2ED573',
+  positive: '#6ECE24',
   negative: '#FF4757',
   warning: '#FFA502',
 };
-
-const Alert: React.FC<AlertProps> = ({ children, kind = 'info', ...rest }) => (
-  <div
+const Alert: React.FC<AlertProps> = ({ children, closed = false, icon = '', kind = 'info', ...rest }) => {
+  const [show, setShow] = useState(true)
+  let cont: any = null;
+  const close = () => {
+    setShow(false)
+  }
+  return (show ? <div
     className={prefixCls}
     style={{
       background: kinds[kind],
     }}
+    ref={e => cont = e}
     {...rest}
   >
+    {
+      icon ? <span className={prefixClsPrev}>{icon}</span> : ''
+    }
     {children}
-  </div>
-);
+    {
+      closed ? <span onClick={close} className={closeCls}>
+        <svg viewBox="64 64 896 896" focusable="false" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"></path></svg>
+      </span> : ''
+    }
+  </div> : null)
+};
 
 Alert.propTypes = {
+  closed: t.bool,
+  icon: t.node,
   kind: t.oneOf(['info', 'positive', 'negative', 'warning']),
 };
 
